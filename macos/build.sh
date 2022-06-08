8670cb57fe5cc52a1c705d0a782856de16fd5228
Bastien Orivel <bastien@technocreatives.com>
2022-06-08 15:41:50 +0200

    Rip out codesigning from build.sh, let CI handle that

--- macos/build.sh
+++ macos/build.sh
@@ -1,13 +1,8 @@
 #!/bin/sh
 set -ex
 
-echo "::add-mask::$MACOS_NOTARIZATION_APP_PWD"
 rm -rf tmp || echo "no tmp dir; continuing"
 rm -rf build || echo "no build dir; continuing"
-
-export MACOS_DEVELOPMENT_TEAM="2K5J2584NX"
-export MACOS_CODE_SIGN_IDENTITY="Developer ID Application: The University of Tromso (2K5J2584NX)"
-export MACOS_CODE_SIGN_IDENTITY_INSTALLER="Developer ID Installer: The University of Tromso (2K5J2584NX)"
 
 PKG_NAME="LibreOfficeOXT.pkg"
 
@@ -27,11 +22,4 @@ pkgbuild --component divvunspell-libreoffice.bundle \
 productbuild --distribution dist.xml \
     --version $VERSION \
     --package-path . \
-    LibreOfficeOXT-unsigned.pkg
-
-productsign --sign "$MACOS_CODE_SIGN_IDENTITY_INSTALLER" LibreOfficeOXT-unsigned.pkg "$PKG_NAME"
-pkgutil --check-signature "$PKG_NAME"
-
-echo "Notarizing installer"
-xcnotary notarize "$PKG_NAME" --override-path-type pkg -d "$MACOS_DEVELOPER_ACCOUNT" -p "$MACOS_NOTARIZATION_APP_PWD"
-stapler validate "$PKG_NAME"
+    $PKG_NAME
